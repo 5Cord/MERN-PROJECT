ea18862bee35af844de4bf377b5235a9737cfe4c
5Cord <dima2v@bk.ru>
2024-04-06 15:43:03 +0300

    uploading images to the server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+import multer from 'multer';
 import checkAuth from "./utils/checkAuth.js"
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
 import * as UserController from './controllers/UserController.js';
 import * as PostController from './controllers/PostController.js';
@@ -11,6 +13,20 @@ mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.seeii9x.mongodb.net/blog?r
     .catch((err) => console.log('DB error', err))
 
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage });
+
+
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -20,6 +36,12 @@ app.get('/', (req, res) => {
 app.post('/login', loginValidation, UserController.login)
 app.post('/register', registerValidation, UserController.register)
 app.get('/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    })
+})
 
 app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.get('/posts', PostController.getAll);
